4b469fe57b1305f7288b2b858b5fd9741d49a2cf
Shiju John <shijujohn1093@gmail.com>
2021-04-01 22:46:00 +0530

    first commit

---
+++ src/main/java/com/javacuckoo/grpcflix/GrpcFlixApplication.java
@@ -0,0 +1,10 @@
+package com.javacuckoo.grpcflix;
+
+//@SpringBootApplication
+public class GrpcFlixApplication {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(GrpcFlixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javacuckoo/grpcflix/GrpcFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacuckoo.grpcflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
